7d648ee52a6b61f942af9d117bd98b031aa73b78
Marija <94226130+mspasovskaa@users.noreply.github.com>
2022-04-09 00:24:30 +0200

    second commit

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL : 'http://localhost:8080',
+    baseURL : 'https://library-backend-emt.herokuapp.com/',
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
